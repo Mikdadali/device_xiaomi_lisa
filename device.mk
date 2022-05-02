93f1e080d8d115411752fa3f573c755af40472f1
Bruno Martins <bgcngm@gmail.com>
2022-05-02 14:06:56 +0100

    lisa: Switch to source-built mlipay interface
    Change-Id: I7daaaffd9fe7a870e42c1d882b52dc93b3231120

--- device.mk
+++ device.mk
@@ -349,6 +349,10 @@ PRODUCT_PACKAGES += \
     libstagefright_softomx.vendor \
     libstagefright_omx.vendor
 
+# Mlipay
+PRODUCT_PACKAGES += \
+    vendor.xiaomi.hardware.mlipay@1.1.vendor
+
 # Network
 PRODUCT_PACKAGES += \
     android.system.net.netd@1.1.vendor
