fa729ab68b0e2784fd4112b7a0731069f363f98f
kubersharma001 <kubersharma001@gmail.com>
2022-08-17 04:07:14 +0000

    lisa: Inherit developer gsi keys
    error:  build/make/target/product/gsi_keys.mk does not exist..
    
    Change-Id: I7ac37f3e104f1bc6dce504ca29fcbf078127b399

--- device.mk
+++ device.mk
@@ -8,8 +8,8 @@
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
-# Installs gsi keys into ramdisk, to boot a GSI with verified boot.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 
 # Enable virtual A/B OTA
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
